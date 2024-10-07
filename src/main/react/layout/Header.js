e259a5cd2dc2e851f4f49111c8ca1b785dba527e
Ihyeon <I2hyeonn@gmail.com>
2024-10-08 01:54:27 +0900

    1008 sim update Main CSS

--- src/main/react/layout/Header.js
+++ src/main/react/layout/Header.js
@@ -1,7 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import '../../resources/static/css/common/Header.css';
+import { FaEnvelope, FaBell, FaCommentDots, FaWindowClose } from 'react-icons/fa'; // React Icons를 사용
 
 function Header() {
+    const [isMessengerOpen, setMessengerOpen] = useState(false);
+
+    const toggleMessenger = () => {
+        setMessengerOpen(!isMessengerOpen); // isMessengerOpen의 현재 값 반전
+    };
 
     return (
         <header>
@@ -9,8 +15,25 @@ function Header() {
                 <div className="logo">
                     <a href="/"><img src="/img/logo2.png" alt="IKEA Logo"/><span>Erpenterprise Resource  Planning</span></a>
                 </div>
+                <div className="header-icons">
+                    <FaEnvelope className="header-icon mail" title="메일" />
+                    <FaCommentDots className="header-icon messenger" title="메신저" onClick={toggleMessenger}/>
+                    <FaBell className="header-icon alarm" title="알림" />
+                </div>
             </div>
             <div className="bottom-border"></div>
+
+            {/*메신저 슬라이드 패널*/}
+            <div className={`messenger-panel ${isMessengerOpen ? 'open' : ''}`}>
+                <div className="messenger-header">
+                    <h2>IKEA TALK</h2>
+                    <FaWindowClose className="messenger-close" title="닫기" onClick={toggleMessenger}></FaWindowClose>
+                </div>
+                <div className="messenger-content">
+                    {/*메신저메신저*/}
+                </div>
+            </div>
+
         </header>
     );
 }
