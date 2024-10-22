bbf29faf275ee27d3e4e9bf3f338c73204de2d91
FORrri <a01056012435@gmail.com>
2024-10-22 11:57:38 +0900

    1022 park

---
+++ src/main/react/components/sales/OrderDispatch.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom/client'; // ReactDOM을 사용하여 React 컴포넌트를 DOM에 렌더링
+import '../../../resources/static/css/common/Main.css'; // 공통 CSS 파일
+import Layout from "../../layout/Layout"; // 공통 레이아웃 컴포넌트를 임포트 (헤더, 푸터 등)
+import { BrowserRouter } from "react-router-dom";
+import '../../../resources/static/css/sales/OrderDispatch.css';
+
+
+function OrderDispatch() {
+
+    return (
+
+
+
+    )
+}
+
+
+//최종 렌더링
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <BrowserRouter>
+        <OrderDispatch />
+    </BrowserRouter>
+);
