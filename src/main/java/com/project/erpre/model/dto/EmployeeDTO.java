8260040af6bbede3010aa769c3f93dc9545df81b
MinSeok Son <aksen1419@gmail.com>
2024-10-24 12:38:56 +0900

    1024 son

--- src/main/java/com/project/erpre/model/dto/EmployeeDTO.java
+++ src/main/java/com/project/erpre/model/dto/EmployeeDTO.java
@@ -19,11 +19,10 @@ public class EmployeeDTO {
     private String employeeEmail;
     private String employeeTel;
     private Integer jobId;
-    private String jobName;
     private String jobRole;
     private Integer departmentId;
 
-    //private String jobName;  // 직급 이름 (읽기용)
+    private String jobName;  // 직급 이름 (읽기용)
     private String departmentName;  // 부서 이름 (읽기용)
 
     private Timestamp employeeInsertDate;
